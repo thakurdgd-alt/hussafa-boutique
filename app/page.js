14c46bafc322fdc49580c0fd6c9c9de1ffe3b5ae
thakurdgd-alt <thakurdgd@gmail.com>
2026-08-08 00:30:26 +0500

    Update page.js

--- app/page.js
+++ app/page.js
@@ -9,7 +9,7 @@ export default function Home() {
           LUXURY FASHION HOUSE
         </p>
 
-        <h1 className="text-5xl md:text-7xl font-bold text-yellow-500 mb-6">
+        <h1 className="text-5xl md:text-7xl font-bold mb-6" style={{color:"#D4AF37"}}>
           HUSSAFA
           <br />
           BOUTIQUE

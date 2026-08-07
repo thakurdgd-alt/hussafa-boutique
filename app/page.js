b938e6a3c128ce12be746d4e9710c9e3df57193d
thakurdgd-alt <thakurdgd@gmail.com>
2026-08-07 22:37:59 +0500

    Update page.js

--- app/page.js
+++ app/page.js
@@ -32,10 +32,13 @@ export default function Home() {
           together in one place.
         </p>
 
-        <button className="bg-yellow-500 text-black px-8 py-3 rounded">
-          Shop Now
-        </button>
-
+    <a href="/shop">
+       <button className="bg-yellow-500 text-black px-8 py-3 rounded">
+         Shop Now
+       </button>
+     </a>
+          
+        
       </section>
 
 
